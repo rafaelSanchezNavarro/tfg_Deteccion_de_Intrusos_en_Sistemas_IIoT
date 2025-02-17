380fb74fad35e67eafc8a32949be855f4c29a726
rafaelSanchezNavarro <rafael.sanchez14@alu.uclm.es>
2025-02-17 20:45:46 +0100

    new changes

--- scripts/preprocesamiento/preprocesamiento_utils/discretizers.py
+++ scripts/preprocesamiento/preprocesamiento_utils/discretizers.py
@@ -1,7 +1,8 @@
 from sklearn.preprocessing import KBinsDiscretizer
+from sklearn.naive_bayes import CategoricalNB
 
 # Definir discretizadores
 discretizers = {
     "k_bins": KBinsDiscretizer(n_bins=5, encode='ordinal', strategy='uniform'),
-    "quantile_bins": KBinsDiscretizer(n_bins=5, encode='ordinal', strategy='quantile')
+    "categoricalNB" : CategoricalNB()
 }
